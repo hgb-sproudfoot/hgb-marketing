eb82a2f06f2632be5d6db24f773dd9de786d4c39
hgb-sproudfoot <simon.proudfoot@hellogbye.com>
2019-02-25 14:07:03 -0500

    SP - add cleaned up version of SVGs for animation to images folder, include all cleaned up inline svgs on page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,11 +22,11 @@ var timelineMain = new TimelineMax({
 });
 
 function itinAnim() {
-    timelineMain.from('#itinerary-path-7, #Line-Copy-3, #itinerary-path-22, #Line-Copy-4, #Date', 0.5, { opacity: 0, ease: Power2.easeIn, x: -200 }, "itinAnim");
+    timelineMain.from('#itinerary-path-7, #Line-Copy-3, #itinerary-path-22, #Line-Copy-4, .itinerary-date', 0.5, { opacity: 0, ease: Power2.easeIn, x: -200 }, "itinAnim");
     timelineMain.from('#Group-7, #Group-5, #Group-4, #Group-5-Copy', 1, { opacity: 0 }, "itinAnim");
     timelineMain.from('#Rectangle-2, #BookNow', 0.5, { opacity: 0, ease: Power1.easeIn, y: 100 }, "itinAnim");
     timelineMain.staggerTo('#Group-7, #Group-5, #Group-4, #Group-5-Copy', 1, { opacity: 0, ease: Power1.easeOut, x: 300 }, .05, "itinAnim+=2");
-    timelineMain.to('#itinerary-path-7, #Line-Copy-3, #itinerary-path-22, #Line-Copy-4, #Date', 1, { opacity: 0 }, "itinAnim+=2");
+    timelineMain.to('#itinerary-path-7, #Line-Copy-3, #itinerary-path-22, #Line-Copy-4, .itinerary-date', 1, { opacity: 0 }, "itinAnim+=2");
     timelineMain.to('#Rectangle-2, #BookNow', 1, { opacity: 0, ease: Power1.easeOut, y: 300 }, "itinAnim+=2");
 };
 
