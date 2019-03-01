9086eb573ca39bbf7e4009e8006942bd9c0455dc
hgb-sproudfoot <simon.proudfoot@hellogbye.com>
2019-03-01 13:30:03 -0500

    SP - run build after merging in second version of itinerary animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -39,9 +39,9 @@ function startAnim() {
     masterTimeline.staggerFrom('#TravelAgent, #TravelReservations', 0.2, { opacity: 0, ease: Power1.easeIn }, 0.1);
     masterTimeline.staggerFrom('.email-mock-text', 0.3, { opacity: 0, width: 0, ease: Power1.easeIn }, 0.03);
     masterTimeline.from('#send-btn', 0.2, { opacity: 0, ease: Power1.easeIn });
-    masterTimeline.from('#send-btn', 0.25, { filter: "url(#dropshadow)" });
-    masterTimeline.to('#send-btn', 0.1, { filter: "url(#inset-shadow)", fill: "#3D83D5", ease: Power1.easeIn });
-    masterTimeline.to('#send-btn', 0.25, { filter: "url(#dropshadow)", fill: "#4A90E2", ease: Power1.easeOut });
+    masterTimeline.from('#send-btn', 0.25, { filter: 'url(#dropshadow)' });
+    masterTimeline.to('#send-btn', 0.1, { filter: 'url(#inset-shadow)', fill: '#3D83D5', ease: Power1.easeIn });
+    masterTimeline.to('#send-btn', 0.25, { filter: 'url(#dropshadow)', fill: '#4A90E2', ease: Power1.easeOut });
     masterTimeline.to('#email', 1, { opacity: 0, ease: Power1.easeOut, x: 300 }, '+=1');
 
     // Itinerary 3 animation
@@ -49,17 +49,17 @@ function startAnim() {
     masterTimeline.from('.itinerary-3-date', 0.5, { opacity: 0, ease: Power2.easeIn, x: -200 }, 'iti32Anim');
     masterTimeline.from('.itinerary-3-card', 1, { opacity: 0 }, 'itin3Anim');
     masterTimeline.from('#itinerary-3 #Button-3', 0.5, { opacity: 0, y: 500, ease: Power1.easeIn }, 'itin3Anim');
-    masterTimeline.from('#itinerary-3 #Rounded-Rectangle', 0.25, { filter: "url(#dropshadow)" });
-    masterTimeline.to('#itinerary-3 #Rounded-Rectangle', 0.1, { filter: "url(#inset-shadow)", fill: "#3F7F9A", ease: Power1.easeIn });
-    masterTimeline.to('#itinerary-3 #Rounded-Rectangle', 0.25, { filter: "url(#dropshadow)", fill: "#4C8CA7", ease: Power1.easeOut });
+    masterTimeline.from('#itinerary-3 #Rounded-Rectangle', 0.25, { filter: 'url(#dropshadow)' });
+    masterTimeline.to('#itinerary-3 #Rounded-Rectangle', 0.1, { filter: 'url(#inset-shadow)', fill: '#3F7F9A', ease: Power1.easeIn });
+    masterTimeline.to('#itinerary-3 #Rounded-Rectangle', 0.25, { filter: 'url(#dropshadow)', fill: '#4C8CA7', ease: Power1.easeOut });
     masterTimeline.staggerTo('.itinerary-3-card', 1, { opacity: 0, ease: Power1.easeOut, x: 300 }, .05, 'itin3Anim+=2.2');
     masterTimeline.to('.itinerary-3-date', 1, { opacity: 0 }, 'itin3Anim+=2.2');
-    masterTimeline.to('#itinerary-3 #Button-3', 1, { opacity: 0, backgroundColor: "#00FFFF", y: 500, ease: Power1.easeOut }, 'itin3Anim+=2.2');
+    masterTimeline.to('#itinerary-3 #Button-3', 1, { opacity: 0, backgroundColor: '#00FFFF', y: 500, ease: Power1.easeOut }, 'itin3Anim+=2.2');
 
     // Slack animation
     masterTimeline.from('#slack', 0.7, { opacity: 0, ease: Power1.easeIn, y: 400 });
     masterTimeline.staggerFrom('.slack-mock-text', 0.3, { opacity: 0, width: 0, ease: Power1.easeOut }, 0.02);
-    masterTimeline.to('#slack', 1, { opacity: 0, ease: Power1.easeOut, x: 300 }, "+=1");
+    masterTimeline.to('#slack', 1, { opacity: 0, ease: Power1.easeOut, x: 300 }, '+=1');
 
     // Itinerary 2 animation
     masterTimeline.from('#itinerary-2 .mock-text', 0.3, { width: 0, ease: Power1.easeIn }, 'itin2Anim')
@@ -70,12 +70,12 @@ function startAnim() {
     masterTimeline.to('#itinerary-2 .has-error .warning-stripe', 0.0, { fill: '#7ED321' });
     masterTimeline.to('#itinerary-2 .has-error .warning-icon', 0.0, { opacity: 0 });
     masterTimeline.to('#itinerary-2 .has-error', 0.7, { opacity: 1, x: 0, ease: Elastic.easeInOut.config(1, 0.7) });
-    masterTimeline.from('#itinerary-2 #Rounded-Rectangle', 0.25, { filter: "url(#dropshadow)" });
-    masterTimeline.to('#itinerary-2 #Rounded-Rectangle', 0.1, { filter: "url(#inset-shadow)", fill: "#3F7F9A", ease: Power1.easeIn });
-    masterTimeline.to('#itinerary-2 #Rounded-Rectangle', 0.25, { filter: "url(#dropshadow)", fill: "#4C8CA7", ease: Power1.easeOut }, "buttonPressed");
-    masterTimeline.staggerTo('.itinerary-2-card', 1, { opacity: 0, ease: Power1.easeOut, x: 300 }, "buttonPressed+=0.3");
-    masterTimeline.to('.itinerary-2-date', 1, { opacity: 0 }, "buttonPressed+=0.3");
-    masterTimeline.to('#itinerary-2 #Button-2', 1, { opacity: 0, backgroundColor: "#00FFFF", y: 500, ease: Power1.easeOut }, "buttonPressed+=0.3");
+    masterTimeline.from('#itinerary-2 #Rounded-Rectangle', 0.25, { filter: 'url(#dropshadow)' });
+    masterTimeline.to('#itinerary-2 #Rounded-Rectangle', 0.1, { filter: 'url(#inset-shadow)', fill: '#3F7F9A', ease: Power1.easeIn });
+    masterTimeline.to('#itinerary-2 #Rounded-Rectangle', 0.25, { filter: 'url(#dropshadow)', fill: '#4C8CA7', ease: Power1.easeOut }, 'buttonPressed');
+    masterTimeline.staggerTo('.itinerary-2-card', 1, { opacity: 0, ease: Power1.easeOut, x: 300 }, 'buttonPressed+=0.3');
+    masterTimeline.to('.itinerary-2-date', 1, { opacity: 0 }, 'buttonPressed+=0.3');
+    masterTimeline.to('#itinerary-2 #Button-2', 1, { opacity: 0, backgroundColor: '#00FFFF', y: 500, ease: Power1.easeOut }, 'buttonPressed+=0.3');
 
     // iPhone animation
     masterTimeline.to('#iPhone', .7, { opacity: 1, ease: Power1.easeOut, y: -700 });
@@ -87,12 +87,12 @@ function startAnim() {
     masterTimeline.from('.itinerary-1-date', 0.5, { opacity: 0, ease: Power2.easeIn, x: -200 }, 'itinAnim');
     masterTimeline.from('.itinerary-1-card', 1, { opacity: 0 }, 'itinAnim');
     masterTimeline.from('#itinerary-1 #Button-1', 0.5, { opacity: 0, y: 500, ease: Power1.easeIn }, 'itinAnim');
-    masterTimeline.from('#itinerary-1 #Rounded-Rectangle', 0.25, { filter: "url(#dropshadow)" });
-    masterTimeline.to('#itinerary-1 #Rounded-Rectangle', 0.1, { filter: "url(#inset-shadow)", fill: "#3F7F9A", ease: Power1.easeIn });
-    masterTimeline.to('#itinerary-1 #Rounded-Rectangle', 0.25, { filter: "url(#dropshadow)", fill: "#4C8CA7", ease: Power1.easeOut });
+    masterTimeline.from('#itinerary-1 #Rounded-Rectangle', 0.25, { filter: 'url(#dropshadow)' });
+    masterTimeline.to('#itinerary-1 #Rounded-Rectangle', 0.1, { filter: 'url(#inset-shadow)', fill: '#3F7F9A', ease: Power1.easeIn });
+    masterTimeline.to('#itinerary-1 #Rounded-Rectangle', 0.25, { filter: 'url(#dropshadow)', fill: '#4C8CA7', ease: Power1.easeOut });
     masterTimeline.staggerTo('.itinerary-1-card', 1, { opacity: 0, ease: Power1.easeOut, x: 300 }, .05, 'itinAnim+=2.2');
     masterTimeline.to('.itinerary-1-date', 1, { opacity: 0 }, 'itinAnim+=2.2');
-    masterTimeline.to('#itinerary-1 #Button-1', 1, { opacity: 0, backgroundColor: "#00FFFF", y: 500, ease: Power1.easeOut }, 'itinAnim+=2.2');
+    masterTimeline.to('#itinerary-1 #Button-1', 1, { opacity: 0, backgroundColor: '#00FFFF', y: 500, ease: Power1.easeOut }, 'itinAnim+=2.2');
     
     
     document.getElementById('animPause').addEventListener('click', function () {
